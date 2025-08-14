e0c60c6fcbec71640a5b8e1b392f3097a6207d9c
kittiphop_earth <kittiphopsompuech2@gmail.com>
2025-08-14 18:02:45 +0700

    update

--- internal/domain/model/customer.go
+++ internal/domain/model/customer.go
@@ -1,15 +1,13 @@
 package model
 
 import (
-	"time"
-
 	"github.com/google/uuid"
 )
 
 type CustomerNote struct {
+	Model
 	ID          uuid.UUID `json:"id" gorm:"type:uuid;default:uuid_generate_v4()"`
-	CustomerID  uuid.UUID `json:"customerId"`
+	CustomerID  string    `json:"customerId"`
 	NoteTypesId uuid.UUID `json:"noteTypeId"`
 	Note        string    `json:"note"`
-	CreatedAt   time.Time `json:"createdAt" gorm:"autoCreateTime"`
 }

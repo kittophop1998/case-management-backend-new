66ec4387ca9554187894c40be4f70ce0cf21a3a9
kittiphop1998 <kittiphopsompuech2@gmail.com>
2025-09-02 23:29:27 +0700

    fix:login access log

--- internal/domain/model/case.go
+++ internal/domain/model/case.go
@@ -113,6 +113,19 @@ type CaseFilter struct {
 	Sort        string     `form:"sort" json:"sort"`
 }
 
+type CreateUpdateUserRequest struct {
+	StaffID      *uint     `gorm:"uniqueIndex" json:"staffId" validate:"required" example:"12337"`
+	Name         string    `json:"name" validate:"required" example:"Janet Adebayo"`
+	Username     string    `gorm:"type:varchar(50)" json:"username" example:"user"`
+	Email        string    `json:"email" validate:"required" example:"Janet@exam.com"`
+	SectionID    uuid.UUID `json:"sectionId" validate:"required" example:"b94eee08-8324-4d4f-b166-d82775553a7e"`
+	OperatorID   *uint     `json:"operatorId" validate:"required" example:"1233"`
+	CenterID     uuid.UUID `json:"centerId" validate:"required" example:"b94eee08-8324-4d4f-b166-d82775553a7e"`
+	RoleID       uuid.UUID `json:"roleId" validate:"required" example:"538cd6c5-4cb3-4463-b7d5-ac6645815476"`
+	DepartmentID uuid.UUID `json:"departmentId" validate:"required" example:"b94eee08-8324-4d4f-b166-d82775553a7e"`
+	IsActive     *bool     `json:"isActive" validate:"required" example:"true"`
+}
+
 type CaseNoteRequest struct {
 	Content string `json:"content"`
 }
